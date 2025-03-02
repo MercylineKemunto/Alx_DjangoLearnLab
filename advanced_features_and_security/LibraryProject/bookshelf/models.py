ff582ec8367a5e30b2845e2d8eb357b197b895f7
MercylineKemunto <mercylinekemunto580@gmail.com>
2025-03-02 14:56:38 +0300

    Implemented permissions and groups for access control

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from relationship_app.models import CustomUser  
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import  BaseUserManager
 
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -17,6 +18,38 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and return a regular user with an email and password."""
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and return a superuser."""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
+    
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()  
+
+    USERNAME_FIELD = 'email'  
+    REQUIRED_FIELDS = ['username']  
+
+    def __str__(self):
+        return self.email
