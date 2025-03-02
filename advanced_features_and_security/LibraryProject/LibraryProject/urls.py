cf7d9f75f3feaf72eb42538727dfde92f50e15e6
MercylineKemunto <mercylinekemunto580@gmail.com>
2025-03-02 22:13:06 +0300

    All my files

--- advanced_features_and_security/LibraryProject/LibraryProject/urls.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
-from .views import form_example_view
-
+from bookshelf.views import form_example_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
