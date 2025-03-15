74e3c0d9195b4688d2c6fcf7e4325fdbbfcdeb4e
MercylineKemunto <mercylinekemunto580@gmail.com>
2025-03-15 20:35:08 +0300

    Implemented DRF Generic Views for Book model

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,18 +2,19 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # ListView: Retrieve all books
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-      permission_classes = [permissions.AllowAny]  # Public access
+    permission_classes = [permissions.AllowAny]  # Public access
 
 # DetailView: Retrieve a single book by ID
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-      permission_classes = [permissions.AllowAny]  # Public access
+    permission_classes = [permissions.AllowAny]  # Public access
 
 # CreateView: Add a new book (Only authenticated users)
 class BookCreateView(generics.CreateAPIView):
