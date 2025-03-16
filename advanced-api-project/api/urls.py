eb2ef39c7bd10aeae5a423e68374dcecddf88ee8
MercylineKemunto <mercylinekemunto580@gmail.com>
2025-03-16 14:46:47 +0300

    All my files

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,13 +1,10 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import AuthorViewSet, BookViewSet
+from django.urls import path
+from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
-# Create a router and register viewsets
-router = DefaultRouter()
-router.register(r'authors', AuthorViewSet, basename='author')
-router.register(r'books', BookViewSet, basename='book')
-
-# Define URL patterns
 urlpatterns = [
-    path('', include(router.urls)),  # Include all registered routes
+    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
 ]
