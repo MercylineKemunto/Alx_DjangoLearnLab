8a8d15d6506e1900f6075f239fa5de436da6928b
MercylineKemunto <mercylinekemunto580@gmail.com>
2025-03-16 14:51:58 +0300

    All my files

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters import rest_framework as filters  
+from django_filters import rest_framework as filters
+from rest_framework.filters import OrderingFilter  
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics
