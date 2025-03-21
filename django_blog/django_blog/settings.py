3ff7aeb547dc89cbf3f2d0f23c68f4039ff7654b
MercylineKemunto <mercylinekemunto580@gmail.com>
2025-03-21 09:49:08 +0300

    Restore manage.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -51,10 +51,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog_db',  
+        'USER': 'admin',  
+        'PASSWORD': 'orina3181',  
+        'HOST': 'localhost',  
+        'PORT': '3306',  
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
