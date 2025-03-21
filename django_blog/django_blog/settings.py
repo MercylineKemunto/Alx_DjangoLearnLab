9ec0329b2f805868adea8bf42fc7de48c6f32fcd
MercylineKemunto <mercylinekemunto580@gmail.com>
2025-03-21 19:51:11 +0300

    Implemented user authentication system

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -86,3 +86,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
