4b0ca42829f8ac47928c725b68fd76f36c9017ce
MercylineKemunto <mercylinekemunto580@gmail.com>
2025-03-23 13:27:23 +0300

    Integrate Tagging Functionality

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Post
+from .models import Post, Tag
 from .models import Comment
 
 class CommentForm(forms.ModelForm):
@@ -20,6 +20,11 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username', 'email']
 class PostForm(forms.ModelForm):
-    class Meta:
+     tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple,  # Enables multiple selection
+        required=False
+    )
+     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
