5c42beafb10c43d177a78d74ba11516d0076ac09
MercylineKemunto <mercylinekemunto580@gmail.com>
2025-03-26 19:38:05 +0300

    social media api setup

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,22 +1,79 @@
-from django.contrib.auth import get_user_model
+# accounts/serializers.py
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True)
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    # Add CharField for password
+    password = serializers.CharField(
+        write_only=True, 
+        required=True, 
+        validators=[validate_password],
+        style={'input_type': 'password'}
+    )
+    password_confirm = serializers.CharField(
+        write_only=True, 
+        required=True, 
+        style={'input_type': 'password'}
+    )
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
+        fields = [
+            'username', 
+            'email', 
+            'password', 
+            'password_confirm', 
+            'first_name', 
+            'last_name'
+        ]
+        extra_kwargs = {
+            'email': {'required': True},
+            'first_name': {'required': False},
+            'last_name': {'required': False}
+        }
+
+    def validate(self, data):
+        """
+        Validate password matching and username uniqueness
+        """
+        if data['password'] != data['password_confirm']:
+            raise serializers.ValidationError({"password": "Passwords do not match."})
+        
+        return data
 
     def create(self, validated_data):
-        """Create a new user with a hashed password and generate a token"""
+        """
+        Create user using create_user method
+        """
+        # Remove confirm password before creating user
+        validated_data.pop('password_confirm')
+        
+        # Use create_user method to create the user
         user = User.objects.create_user(
             username=validated_data['username'],
-            email=validated_data.get('email', ''),  # Optional email
-            password=validated_data['password']
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
         )
-        Token.objects.create(user=user)  # Create an authentication token for the user
+        
+        # Create an authentication token for the user
+        Token.objects.create(user=user)
+        
         return user
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id', 
+            'username', 
+            'email', 
+            'first_name', 
+            'last_name'
+        ]
+        read_only_fields = ['id', 'username']
