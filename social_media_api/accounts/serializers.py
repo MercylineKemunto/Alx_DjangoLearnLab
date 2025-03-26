4c921561f463a39257d43c5892dc2fb8794b3f6b
MercylineKemunto <mercylinekemunto580@gmail.com>
2025-03-26 19:44:44 +0300

    social media api setup

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
+# Import Token model
+from rest_framework.authtoken.models import Token
 
 # Ensure we're using the custom user model
 User = get_user_model()
@@ -18,6 +20,8 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         required=True, 
         style={'input_type': 'password'}
     )
+    # Add a token field to return the authentication token
+    token = serializers.CharField(read_only=True)
 
     class Meta:
         model = User
@@ -25,7 +29,8 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             'username', 
             'email', 
             'password', 
-            'password_confirm'
+            'password_confirm',
+            'token'
         ]
 
     def validate(self, data):
@@ -34,23 +39,29 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         """
         if data['password'] != data['password_confirm']:
             raise serializers.ValidationError({
-                "password": "Passwords do not match corrrectly."
+                "password": "Passwords do not match."
             })
         return data
 
     def create(self, validated_data):
         """
-        Create user using create_user method corrrectly
+        Create user and generate authentication token
         """
         # Remove password_confirm before creating user
         validated_data.pop('password_confirm')
 
-        # Use create_user method to create the user corrrectly
+        # Create user using create_user method
         user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
+
+        # Create a token for the user
+        token = Token.objects.create(user=user)
+
+        # Attach the token to the user instance for serializer response
+        user.token = token.key
 
         return user
 
